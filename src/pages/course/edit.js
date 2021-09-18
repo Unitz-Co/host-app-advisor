a822d93acff3d771ab066a83c567a73b2947795f
Hung Tran <hungtran0203@gmail.com>
2021-09-19 00:07:40 +0700

    course

--- src/pages/course/edit.js
+++ src/pages/course/edit.js
@@ -7,7 +7,7 @@ import PageData from '../../data/PageDataQuery';
 import DIV from '@vl/redata/DIV.macro';
 import SEO from '@uz/unitz-layout-web/SEO';
 import { ctx } from '@vl/redata';
-import CourseListing from '@uz/unitz-pages-advisor/CourseListing';
+import CourseEdit from '@uz/unitz-pages-advisor/CourseEdit';
 
 const Index = withPageContext((props) => {
   return (
@@ -18,7 +18,7 @@ const Index = withPageContext((props) => {
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'advisorNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
             <div className="min-h-full bg-background1">
-              <CourseListing />
+              <CourseEdit />
             </div>
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
