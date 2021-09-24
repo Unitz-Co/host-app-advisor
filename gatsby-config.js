63db41c8459b7c0a09f6911189fa12af4133bcc9
Hung Tran <hungtran0203@gmail.com>
2021-09-24 22:26:41 +0700

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ const gbConfig = gatsbyMergeConfig(
       'gatsby-transformer-remark',
       'gatsby-transformer-sharp',
       'gatsby-plugin-react-helmet',
-      'gatsby-plugin-sharp',
+      // 'gatsby-plugin-sharp',
       'gatsby-plugin-postcss',
       'gatsby-plugin-sass',
       // {
