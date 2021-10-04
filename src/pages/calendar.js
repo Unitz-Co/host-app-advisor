13669505e5deba2fb9c33e7dbeef8e77ef2e4aa1
Si Truong <si.truongtan@gmail.com>
2021-10-04 14:38:05 +0700

    Use new type fullpage layout

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import withPageContext from '@uz/unitz-pages/withPageContext';
 import App from '@uz/unitz-app-web/AdvisorApp';
-import Layout from '@uz/unitz-layout-web/LayoutAdvisor';
+import Layout from '@uz/unitz-layout-web/LayoutFullPage';
 import PageData from '../data/PageDataQuery';
 import DIV from '@vl/redata/DIV.macro';
 import SEO from '@uz/unitz-layout-web/SEO';
@@ -12,14 +12,12 @@ import Calendar from '@uz/unitz-pages-advisor/Calendar';
 const CalendarIndex = withPageContext((props) => {
   return (
     <App>
-      <Layout location={props.location} PageData={PageData} fullScreen>
+      <Layout location={props.location} PageData={PageData}>
         <DIV>
           <SEO pageData={ctx.apply('ctf.findPage', { name: 'Homepage' })} />
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'advisorNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
-            {/* <div className=" bg-background1"> */}
             <Calendar />
-            {/* </div> */}
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'articleFooterSection' })}</Layout.POS>
         </DIV>
