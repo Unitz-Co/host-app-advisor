b62d05861fa59b40a39f7ca0b484dc6d8ba23610
Hung Tran <hungtran0203@gmail.com>
2021-12-30 21:11:09 +0700

    advisor registration via google form

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -7,12 +7,23 @@ import SignupPage from '@uz/unitz-pages-advisor/SignupPage';
 
 import App from '@uz/unitz-app-web/AdvisorApp';
 import SEO from '@uz/unitz-layout-web/SEO';
+import useRoute from '@vl/hooks/useGbRoute';
+import LoadingScreen from '@uz/unitz-components-web/LoadingScreen';
 
 import Layout from '@uz/unitz-layout-web/LayoutMain';
 import withPageContext from '@uz/unitz-pages/withPageContext';
 import PageData from '../data/PageDataQuery';
 
+const registerUrl = 'https://bit.ly/Unitzform';
+
 const RegisterIndex = withPageContext((props) => {
+  const route = useRoute();
+  React.useEffect(() => {
+    route.navigateExternal(registerUrl);
+  }, []);
+  if (registerUrl) {
+    return <LoadingScreen />;
+  }
   return (
     <App>
       <Layout location={props.location} PageData={PageData}>
