0cc59765022fb3fa120be64b61822892d20186a8
Phuong Vo <vlaphuong1991@gmail.com>
2022-01-06 16:03:54 +0700

    update

--- src/templates/advisor/.route.js
+++ src/templates/advisor/.route.js
@@ -26,3 +26,18 @@ routeStore.addRule('advisor:user', {
     return `${routeStore.getAppOrigin('user')}${_.get(params, 'profile.slug')}`;
   },
 });
+routeStore.addRule('advisor/profile', {
+  url: (params) => {
+    return `/advisor/profile?id=${_.get(params, 'id')}`;
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'advisor/profile';
+  },
+});
