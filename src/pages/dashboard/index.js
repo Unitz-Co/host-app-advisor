ed85b75827b051cdd1c0fbfa6847d5ef1934d209
Phuong Vo <vlaphuong1991@gmail.com>
2022-01-26 12:05:06 +0700

    update

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -7,7 +7,7 @@ import PageData from '../../data/PageDataQuery';
 import DIV from '@vl/redata/DIV.macro';
 import SEO from '@uz/unitz-layout-web/SEO';
 import { ctx } from '@vl/redata';
-import Profile from '@uz/unitz-pages-advisor/AdvisorDashBoard';
+import AdvisorDashBoard from '@uz/unitz-pages-advisor/AdvisorDashBoard';
 
 const Index = withPageContext((props) => {
   return (
@@ -18,7 +18,7 @@ const Index = withPageContext((props) => {
           <Layout.POS name="app-header">{ctx.apply('ctf.renderSection', { name: 'advisorNavbarSection' })}</Layout.POS>
           <Layout.POS name="app-body">
             <div className="min-h-full bg-background1">
-              <Profile />
+              <AdvisorDashBoard />
             </div>
           </Layout.POS>
           <Layout.POS name="app-footer">{ctx.apply('ctf.renderSection', { name: 'FooterSection' })}</Layout.POS>
