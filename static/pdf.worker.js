837d03fb69ce36dcdc9129cd9ec6ea75f0311565
Phuong Vo <vlaphuong1991@gmail.com>
2022-02-11 10:59:01 +0700

    update pdf.worker.js

--- static/pdf.worker.js
+++ static/pdf.worker.js
@@ -1,3 +1,3 @@
-import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf'
-import PDFJSWorker from 'pdfjs-dist/legacy/build/pdf.worker.min'
+import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf';
+import PDFJSWorker from 'pdfjs-dist/legacy/build/pdf.worker.entry';
 pdfjsLib.GlobalWorkerOptions.workerSrc = PDFJSWorker;
