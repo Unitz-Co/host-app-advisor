95d6662b67e305fa8af4ab5cee9915503c9d74c1
anhphong-2010 <anhphongnw.2010@gmail.com>
2022-07-27 10:36:38 +0700

    -  google docs link

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -14,23 +14,23 @@ import Layout from '@uz/unitz-layout-web/LayoutMain';
 import withPageContext from '@uz/unitz-pages/withPageContext';
 import PageData from '../data/PageDataQuery';
 
-const registerUrlVN =
-  'https://docs.google.com/forms/d/e/1FAIpQLScrAH5U43xN6aYElG5HoFOW-lO1qBSBg_NVUec-uXDgOgETKw/viewform';
+// const registerUrlVN =
+//   'https://docs.google.com/forms/d/e/1FAIpQLScrAH5U43xN6aYElG5HoFOW-lO1qBSBg_NVUec-uXDgOgETKw/viewform';
 
-const registerUrlEN =
-  'https://docs.google.com/forms/d/e/1FAIpQLSe9jLH2vAxJZCLCgY8OBJoi-VpbxVOY3SnCj0oQa12S5Wvo5g/viewform';
+// const registerUrlEN =
+//   'https://docs.google.com/forms/d/e/1FAIpQLSe9jLH2vAxJZCLCgY8OBJoi-VpbxVOY3SnCj0oQa12S5Wvo5g/viewform';
 
 const RegisterIndex = withPageContext((props) => {
-  const route = useRoute();
-  const lang = _.get(route.getPageContext(), 'lang', 'vi');
-  React.useEffect(() => {
-    if (lang === 'vi') {
-      route.navigateExternal(registerUrlVN);
-    } else if (lang === 'en') route.navigateExternal(registerUrlEN);
-  }, []);
-  if (registerUrlVN || registerUrlEN) {
-    return <LoadingScreen />;
-  }
+  // const route = useRoute();
+  // const lang = _.get(route.getPageContext(), 'lang', 'vi');
+  // React.useEffect(() => {
+  //   if (lang === 'vi') {
+  //     route.navigateExternal(registerUrlVN);
+  //   } else if (lang === 'en') route.navigateExternal(registerUrlEN);
+  // }, []);
+  // if (registerUrlVN || registerUrlEN) {
+  //   return <LoadingScreen />;
+  // }
 
   return (
     <App>
